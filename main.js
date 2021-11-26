16efdd24b69e263d7f8ba09e3134d8ce5fd454f7
Vladyslav <frontendvlados@gmail.com>
2021-11-26 23:21:51 +0200

    changes at HTML file and do second task

--- main.js
+++ main.js
@@ -1,3 +1,5 @@
+//Задание № 1
+
 function rangeNumbers(a, b) {
     if (a === b) {
         console.log(a);
@@ -15,11 +17,45 @@ rangeNumbers(1, 13);
 
 
 
+//Задание № 2
+
+
+let hoursBlock = document.getElementById('hours');
+let minutesBlock = document.getElementById('minutes');
+let secondsBlock = document.getElementById('seconds');
+let dotsOne = document.getElementById('dotsOne');
+let dotsSecond = document.getElementById('dotsSecond');
+
+        
+setInterval(function() {
+    let time = new Date(),
+        hours = (time.getHours() < 10) ? '0' + time.getHours() : time.getHours(),
+        minutes = (time.getMinutes() < 10) ? '0' + time.getMinutes() : time.getMinutes(),
+        seconds = (time .getSeconds() < 10) ? '0' + time.getSeconds() : time.getSeconds();
+
+    hoursBlock.innerHTML = hours;
+    minutesBlock.innerHTML = minutes;
+    secondsBlock.innerHTML = seconds;
+    dotsOne.innerHTML = ':';
+    dotsSecond.innerHTML = ':';
+
+}, 250)
+
+document.getElementById('btn').addEventListener('click', function(event) {
+    if (event.target.tagName === 'BUTTON') {
+        dotsSecond.classList.toggle('displayDots');
+        secondsBlock.classList.toggle('displaySeconds');
+    } else {
+        dotsSecond.innerHTML = ':';
+        secondsBlock.innerHTML = seconds;
+    }
+})
 
 
 
 
 
+    
 
 
 
